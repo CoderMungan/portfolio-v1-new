611409cc8704ed1391af988966435aa73150d6b9
CoderMungan <codermungan@gmail.com>
2023-11-21 12:50:45 +0300

    Docker Setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-327xure%p(^!)sbdbw^*)az=n06abh^+2+-$&sjquuatfmnv=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
