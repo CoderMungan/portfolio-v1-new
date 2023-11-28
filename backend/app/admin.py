5da28d2484ea5eaf801a3e4681a050d90355ef46
CoderMungan <codermungan@gmail.com>
2023-11-28 15:08:20 +0300

    Update: Category, Blog and Label add to admin panel

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(CategoryBlog)
-admin.site.register(Blog)
+class CategoryBlogAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+
+admin.site.register(LabelCategory)
+admin.site.register(CategoryBlog, CategoryBlogAdmin)
+admin.site.register(Blog, BlogAdmin)
