9a1fd753957ad9f2b61da20dc4bf4099a4985a5a
CoderMungan <codermungan@gmail.com>
2023-11-28 12:53:22 +0300

    Add: Create Blog Model

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(CategoryBlog)
+admin.site.register(Blog)
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class CategoryBlog(models.Model):
+    name = models.CharField(("Category Name"), max_length=50)
+    slug = models.SlugField(("Slug"))
+
+
+class Blog(models.Model):
+    name = models.ForeignKey(CategoryBlog, verbose_name=("Category Name"), on_delete=models.CASCADE)
+    title = models.CharField(("Title"), max_length=200)
+    content = models.TextField(("Content"))
+    createAt = models.DateTimeField(("Create At"), auto_now=True, auto_now_add=True)
+
