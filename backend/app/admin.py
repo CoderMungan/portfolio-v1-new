b198a9d69d5e99f9a761d533f97b7f1def0f8df6
CoderMungan <codermungan@gmail.com>
2023-11-29 09:31:37 +0300

    Update: Article + CategoryArticle register

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+
 class CategoryBlogAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+class CategoryArticleAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+
 admin.site.register(LabelCategory)
 admin.site.register(CategoryBlog, CategoryBlogAdmin)
+admin.site.register(CategoryArticle, CategoryArticleAdmin)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Article)
