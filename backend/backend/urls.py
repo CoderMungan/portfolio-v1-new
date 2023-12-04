47af24524462d98ad2d96921b5f667fda8f5ca54
CoderMungan <codermungan@gmail.com>
2023-12-04 06:17:49 +0300

    Add: home page from api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -45,6 +45,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index),
     path('api/', include('app.urls')),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
