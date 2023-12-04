793303bd37af88c3f9302a263bb50ee3db58c79b
CoderMungan <codermungan@gmail.com>
2023-12-04 14:44:51 +0300

    Add: blog & singleblog page

---
+++ client/src/Pages/BlogSections.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+
+export default function BlogSections() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const apiRequest = async () => {
+            const request = await fetch("http://127.0.0.1:8000/api/blog")
+            const response = await request.json()
+            setPosts(response)
+            console.log("gelen api:", response);
+        }
+        apiRequest()
+    }, [])
+
+    return (
+        <>
+            <section className="bg-white dark:bg-gray-900">
+                <div className="py-8 px-4 mx-auto max-w-screen-lg lg:py-16 lg:px-6">
+                    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                        <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
+                        <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+                    </div>
+                    <div className="grid gap-8 lg:grid-cols-2">
+                        {posts.map((post) => (
+                            <article key={post.id} className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 h-72 overflow-hidden">
+                                <div className="flex justify-between items-center mb-5 text-gray-500">
+                                    <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                        <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
+                                        </svg>
+                                        {post.category}
+                                    </span>
+                                    <span className="text-sm">{post.createAt}</span>
+                                </div>
+                                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    <Link to={post.slug} className="hover:underline">
+                                        {post.title}
+                                    </Link>
+                                </h2>
+                                <p className="mb-5 font-light text-gray-500 dark:text-gray-400 line-clamp-3">
+                                    {post.content}
+                                </p>
+                                <div className="flex justify-between items-center">
+                                    <Link to={post.slug} className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                                        Devamını Oku
+                                        <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                        </svg>
+                                    </Link>
+                                </div>
+                            </article>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ client/src/Pages/SingleBlog.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { IoMdArrowRoundBack } from 'react-icons/io'
+import { Link, useParams } from 'react-router-dom'
+
+
+export default function SingleBlog() {
+    const { slug } = useParams()
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const apiRequest = async () => {
+            const request = await fetch(`http://127.0.0.1:8000/api/blog/${slug}`)
+            const response = await request.json()
+            setData(response)
+        }
+        apiRequest()
+    }, [])
+
+    return (
+        <>
+            <Link to="/blog">
+                <p className="max-w-screen-md mx-auto mt-10 text-2xl">
+                    <IoMdArrowRoundBack />
+                </p>
+            </Link>
+            {data && (
+                <div key={data.id} className="px-4 lg:px-0 mt-10 max-w-screen-md mx-auto text-lg leading-relaxed">
+                    <h1 className="text-center font-bold text-5xl">{data.title}</h1>
+                    <div className="flex justify-between text-gray-600 mt-10 mb-5">
+                        <p className="text-center text-sm">{data.createAt}</p>
+                        <p className="text-center text-sm">{data.category}</p>
+                    </div>
+                    <div className="max-w-screen-md">
+                        <p className="pb-6">{data.content}</p>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
