f18862a54b8f8a502961512237a6b8e5c39bbad1
CoderMungan <codermungan@gmail.com>
2023-12-04 14:43:50 +0300

    Add: Blog and singleblog path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import ArticleSections from './Pages/ArticleSections';
 import SingleArticle from './Pages/SingleArticle';
 import ContactForm from './Pages/ContactForm';
 import ContactSucces from './Pages/ContactSucces';
+import BlogSections from './Pages/BlogSections';
+import SingleBlog from './Pages/SingleBlog';
 
 
 
@@ -21,6 +23,8 @@ function App() {
         <Route path='/' element={<HeaderComponent></HeaderComponent>}></Route>
         <Route path='/article' element={<ArticleSections></ArticleSections>}></Route>
         <Route path='/article/:slug' element={<SingleArticle></SingleArticle>}></Route>
+        <Route path='/blog/' element={<BlogSections></BlogSections>}></Route>
+        <Route path='/blog/:slug' element={<SingleBlog></SingleBlog>}></Route>
         <Route path='/contact-me' element={<ContactForm></ContactForm>}></Route>
         <Route path='/form-succes' element={<ContactSucces></ContactSucces>}></Route>
         <Route path='*' element={<PageNotFound404></PageNotFound404>}></Route>
