4d680ee9098e156a4f6fa7f90f29fd13f5dcfb4a
CoderMungan <codermungan@gmail.com>
2023-12-04 04:19:00 +0300

    Add: config drf_yasg

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,16 +17,35 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from rest_framework.schemas import get_schema_view
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 from app.views import *
 
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Portfolio Api",
+        default_version='v1',
+        description="Portfoliocuk",
+        terms_of_service="localde",
+        contact=openapi.Contact(email="admin@gmail.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
-    path('category', category_response_get),
-    path('blog', blog_main_response_get),
-    path('blog/<slug>', blog_special_theme_get),
-    path('article/<slug>', article_special_theme_get),
-    path('article', article_main_response_get),
+    path("admin/", admin.site.urls),
+    path('api/', include('app.urls')),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
